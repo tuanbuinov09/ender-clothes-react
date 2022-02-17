922fb5c7979b906b2bf60f9856b549ebedcdbaa0
Tuan EnderMan <t01684599820@gmail.com>
2022-02-17 19:04:56 +0700

    header

--- src/components/Content/Item/Item.js
+++ src/components/Content/Item/Item.js
@@ -4,44 +4,46 @@ import {
 import React from 'react';
 import Icon from 'react-hero-icon';
 import style from './Item.module.css';
-function Item(props) {
-    if (props.type === 1) {
+import clsx from "clsx";
+function Item({ product, type }) {
+    if (type === 1) {
         return (
-            <div className="item col-1per5 p-x-16">
-                <Link to={`/products/${props.product.id}`} className="item__img-container">
-                    <img src={props.product.image} alt={`product:${props.product.id}`} />
-                </Link>
-                <Link to={`/products/${props.product.id}`} className="item__label">{props.product.title}</Link>
-                <div className="bottom-of-item-container">
-                    <div className="bottom-of-item"><p className="item__price"><span>{props.product.price} $</span> <Link to={`/add-to-bag?productId=${props.product.id}`} className="header-icon shopping-bag-icon"><Icon icon="shopping-bag" className="header-icon-svg" /><b className="plus-text">+</b></Link></p>
-                        {/* <div className="add-to-cart-button">Add to cart</div> */}
-                    </div>
+            <div className={clsx(style.item)} >
+                <div>
+                    <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
+                        <img src={product.image} alt={`product:${product.id}`} />
+                    </Link>
+                    <Link to={`/products/${product.id}`} className={clsx(style.label)}>{product.title}</Link>
                 </div>
+
+                <p className={clsx(style.price)}><span>{product.price} $</span></p>
+            </div >
+        );
+    }
+    if (type === 2) {
+        return (
+            <div className={clsx(style.item, style.item2)} >
+                <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
+                    <img src={product.image} alt="item" />
+                </Link>
             </div>
         );
     }
-    if (props.type === 2) {
+    if (type === 3) {
         return (
-            <div className="item sale-off__img-container col-third">
-                <Link to={`/products/${props.product.id}`} className="item__img-container">
-                    <img src={props.product.image} alt="item" />
-                </Link>
-            </div>
-        );
-    }
-    if (props.type === 3) {
-        return (
-            <div className="item col-1per5 p-x-16">
-                <Link to={`/products/${props.product.id}`} className="item__img-container">
-                    <img src={props.product.image} alt="item" />
-                </Link>
-                <Link to={`/products/${props.product.id}`} className="item__label">{props.product.title}</Link>
-                <div className="bottom-of-item-container">
-                    <div className="bottom-of-item"><p className="item__price"><span>{props.product.price} $</span> <Link to={`/add-to-bag?productId=${props.product.id}`} className="header-icon shopping-bag-icon"><Icon icon="shopping-bag" className="header-icon-svg" /><b className="plus-text">+</b></Link></p>
-                        {/* <div className="add-to-cart-button">Add to cart</div> */}
-                    </div>
+            <div className={clsx(style.item, style.item3)}>
+                <div>
+                    <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
+                        <img src={product.image} alt="item" />
+                    </Link>
+                    <Link to={`/products/${product.id}`} className={clsx(style.label)}>{product.title}</Link>
                 </div>
-                <Link to={`/products/${props.product.id}`} className="btn bg-dark">SEE DETAILS</Link>
+                <div>
+                    <p className={clsx(style.price)}><span>{product.price} $</span></p>
+                    {/* <div className="add-to-cart-button">Add to cart</div> */}
+                    <Link to={`/products/${product.id}`} className="btn bg-dark">SEE DETAILS</Link>
+                </div>
+
             </div>
         );
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,24 @@
 import Header from "./Header/Header";
 import Content from "./Content/Content";
 import Footer from "./Footer";
+import About from "./AboutPage/About"
+import {
+    BrowserRouter, Route, Routes,
+} from "react-router-dom";
 function Main() {
     return (
-        <div className="main">
-            <Header />
-            <Content />
-            <Footer />
-        </div>
+        <BrowserRouter>
+            <div className="main">
+                <Header />
+                <Routes>
+                    <Route path="" element={<Content />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+
+                <Footer />
+
+            </div>
+        </BrowserRouter>
     );
 }
 
