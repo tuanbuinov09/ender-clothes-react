75af55b2108029c44c9563a05ba82064f7b04293
tuanbuinov09 <tuanbuinov09@gmail.com>
2022-04-11 23:12:29 +0700

    update item

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Main from './components/Main';
 function App() {
   return (
     <Main />
-  );
+  ); 
 }
 
 export default App;
--- src/components/HomePage/Item/Item.js
+++ src/components/HomePage/Item/Item.js
@@ -2,16 +2,21 @@ import {
     Link
 } from "react-router-dom";
 import React from 'react';
-// import Icon from 'react-hero-icon';
+import Icon from 'react-hero-icon';
 import style from './Item.module.css';
 import clsx from "clsx";
 function Item({ product, type }) {
     if (type === 1) {
         return (
             <div className={clsx(style.item, style.type1)} >
+                <div className={clsx(style.itemMenu)}>
+                    <div className={clsx(style.iconContainer)}><Icon icon="shopping-bag" type="solid" className={clsx(style.iconSvg)} /></div>
+                    <div className={clsx(style.iconContainer)}><Icon icon="heart" type="solid" className={clsx(style.iconSvg)} /></div>
+                </div>
                 <div>
                     <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
                         <img src={product.image} alt={`product:${product.id}`} />
+
                     </Link>
                     <Link to={`/products/${product.id}`} className={clsx(style.label)}>{product.title}</Link>
                 </div>
@@ -23,6 +28,10 @@ function Item({ product, type }) {
     if (type === 2) {
         return (
             <div className={clsx(style.item, style.type2)} >
+                <div className={clsx(style.itemMenu)}>
+                    <div className={clsx(style.iconContainer)}><Icon icon="shopping-bag" type="solid" className={clsx(style.iconSvg)} /></div>
+                    <div className={clsx(style.iconContainer)}><Icon icon="heart" type="solid" className={clsx(style.iconSvg)} /></div>
+                </div>
                 <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
                     <img src={product.image} alt="item" />
                 </Link>
@@ -32,6 +41,10 @@ function Item({ product, type }) {
     if (type === 3) {
         return (
             <div className={clsx(style.item, style.type3)}>
+                <div className={clsx(style.itemMenu)}>
+                    <div className={clsx(style.iconContainer)}><Icon icon="shopping-bag" type="solid" className={clsx(style.iconSvg)} /></div>
+                    <div className={clsx(style.iconContainer)}><Icon icon="heart" type="solid" className={clsx(style.iconSvg)} /></div>
+                </div>
                 <div>
                     <Link to={`/products/${product.id}`} className={clsx(style.imgContainer)}>
                         <img src={product.image} alt="item" />
