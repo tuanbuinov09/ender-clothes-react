5411d9cf6bfee4084419583445dc7440697ce929
tuanbuinov09 <tuanbuinov09@gmail.com>
2022-10-17 01:41:01 +0700

    update hinh anh

--- src/components/ProductDetail/EJ2ImageCarousel.js
+++ src/components/ProductDetail/EJ2ImageCarousel.js
@@ -1,21 +1,26 @@
 import { CarouselComponent } from "@syncfusion/ej2-react-navigations";
+import { removeSyncfusionLicenseMessage } from "../../uitilities/utilities";
 import * as React from "react";
 class EJ2ImageCarousel extends React.Component {
     constructor() {
         super();
         this.showButtons = "VisibleOnHover";
-        
+        removeSyncfusionLicenseMessage();
     }
     getSelectedIndex() {
         return this.props.productImages.findIndex((item) => {
             return item.MA_MAU === this.props.selectedProductDetail.MA_MAU
         })
     }
-    
+    setIsShowButton() {
+        return this.showButtons = this.props.productImages.length>1?"VisibleOnHover":"Hidden";
+    }
     itemTemplate(img) {
         return <figure className="img-container"><img src={img.HINH_ANH} alt={img.TEN_MAU} style={{ height: "100%", width: "100%" }} /></figure>;
     }
-
+componentDidMount(){
+    removeSyncfusionLicenseMessage();
+}
     componentDidUpdate(){
         console.log('updated')
     }
@@ -24,12 +29,30 @@ class EJ2ImageCarousel extends React.Component {
         console.log('unmount')
     }
 
+    onSlideChanging(args) {
+        console.log(args.currentSlide); // You can customize the slide before changing.
+    }
+    onSlideChanged(args) {
+        console.log(args.currentSlide); // You can customize the slide after changed.
+    }
+
     render() {
         console.log(this.props.productImages)
         console.log(this.getSelectedIndex())
         return (
             <div className='control-container'>
-                <CarouselComponent buttonsVisibility={this.showButtons} selectedIndex={this.getSelectedIndex()} dataSource={this.props.productImages} itemTemplate={this.itemTemplate.bind(this)}></CarouselComponent>
+                <CarouselComponent autoPlay={this.props.productImages.length>1?true:false} 
+                buttonsVisibility={this.setIsShowButton()} 
+                selectedIndex={this.getSelectedIndex()} 
+                dataSource={this.props.productImages} 
+                itemTemplate={this.itemTemplate.bind(this)}
+                pauseOnHover={true}
+                interval={7000} 
+                loop={true}
+                slideChanging={this.onSlideChanging.bind(this)} slideChanged={this.onSlideChanged.bind(this)}
+                enableTouchSwipe={this.props.productImages.length>1?true:false}>
+
+                </CarouselComponent>
             </div>);
     }
 }
